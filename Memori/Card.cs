01d6f6c2e4f7dffdcbe85f217027bf0c546e91a4
Soth-Yogoth <midnight746@yandex.ru>
2023-07-08 17:29:21 +0700

    Add project files.

---
+++ Memori/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Memori
+{
+    public class Card : Button
+    {
+        MediaPlayer clickSound = new MediaPlayer();
+        Image face = new Image();
+        Image back = new Image();    
+
+        public Card(int i) : base()
+        {
+            Margin = new Thickness(5);
+
+            face.Source = new BitmapImage(new Uri($"pack://application:,,,/Resources/{i}.jpg"));
+            back.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/0.jpg"));
+
+            clickSound.Open(new Uri("Resources\\Щелчёк.mp3", UriKind.Relative));
+        }
+
+        protected override void OnClick()
+        {         
+            FaceUp();
+            clickSound.Stop();
+            clickSound.Play();
+            base.OnClick();   
+        }
+
+        public void FaceUp()
+        {
+            IsEnabled = false;
+            Content = face;         
+        }
+
+        public void FaceDown()
+        {
+            IsEnabled = true;           
+            Content = back;        
+        }
+
+        public static bool Equals(Card a, Card b)
+        {
+            if(a.face.Source.ToString() == b.face.Source.ToString()) return true;
+            else return false;
+        }
+    }
+}
---
+++ Memori/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Memori
+{
+    public partial class MainWindow : Window
+    {
+        List<Card> Cards = new List<Card>();
+        Card? selectedCard;
+
+        MediaPlayer music = new MediaPlayer();
+        MediaPlayer timerSound = new MediaPlayer();
+        MediaPlayer gameOverSound = new MediaPlayer();
+        
+
+        DispatcherTimer timer = new DispatcherTimer();
+        Stopwatch stopwatch = new Stopwatch();
+        TimeSpan timeSpan;
+
+        bool flipped;
+        int movesCount;
+        int pairsFound;
+
+        public MainWindow()
+        {
+            for (int j = 1; j < 7; j++)
+            {
+                Cards.Add(new Card(j));
+                Cards.Add(new Card(j));
+            }
+
+            foreach (Card card in Cards) 
+            { 
+                card.Click += CheckPair;
+            }
+
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+            timerSound.Open(new Uri("Resources\\Таймер.mp3", UriKind.Relative));
+            music.Open(new Uri("Resources\\Музыка.mp3", UriKind.Relative));
+            music.MediaEnded += new EventHandler(Media_Ended);
+            music.Play();
+
+            InitializeComponent();
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            music.Stop();
+            music.Play();
+        }
+
+        private async void CheckPair(object sender, RoutedEventArgs e)
+        {
+            Card card = (Card)sender;
+
+            if (selectedCard == null) selectedCard = card;
+            else
+            {
+                if (!Card.Equals(card, selectedCard))
+                {
+                    Table.IsEnabled = false;
+                    await Task.Delay(500);
+                    card.FaceDown();
+                    selectedCard.FaceDown();
+                    Table.IsEnabled = true;
+                }
+                else pairsFound++;
+                
+                selectedCard = null;
+                movesCount++;
+                Counter.Text = $"Ходов следано: {movesCount}";
+            }
+
+            if (pairsFound == 6) GameOver("Поздравляю, Вы нашли все пары!", true);
+        }
+
+        private void ShuffleCards()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 12; i++) 
+            {
+                int k = random.Next(12);
+                Card card = Cards[i];
+                Cards[i] = Cards[k];
+                Cards[k] = card;
+            }
+        }
+
+        private void PutCards()
+        {
+            Table.Children.Clear();
+
+            foreach (Card card in Cards) 
+            { 
+                Table.Children.Add(card);
+            }
+        }
+
+        private void ShowCards()
+        {
+            foreach (Card card in Cards)
+                card.FaceUp();
+        }
+
+        private void HideCards()
+        {
+            foreach (Card card in Cards)
+                card.FaceDown();
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            Counter.Text = "Ходов сделано: 0";
+            movesCount = 0;
+            pairsFound = 0;
+
+            stopwatch.Restart();
+            timer.Start();
+
+            ShuffleCards();            
+            PutCards();    
+            ShowCards();
+            flipped = false;
+
+            Table.Visibility = Visibility.Visible;
+            Table.IsEnabled = true;
+        }
+
+        private void timer_Tick(object obj, EventArgs e)
+        {
+            timeSpan = new TimeSpan(0, 0, (int)(61 - stopwatch.Elapsed.TotalSeconds));
+            Time.Text = String.Format("{0:00}:{1:00}:{2:00}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+            if(timeSpan.TotalSeconds < 60 && !flipped)
+            {
+                HideCards();
+                flipped = true;
+            }
+
+            if(timeSpan.TotalSeconds < 16 && timerSound.Position == TimeSpan.Zero)
+            {   
+                timerSound.Play();
+            }
+
+            if (timeSpan.TotalSeconds < 1)
+            {             
+                Table.IsEnabled = false;
+                GameOver("Время вышло! Вы проиграли.", false);
+            }
+        }
+
+        private void GameOver(string mes, bool victory)
+        {
+            selectedCard = null;
+
+            timerSound.Stop();
+            timer.Stop();
+            stopwatch.Stop();
+
+            if (victory) gameOverSound.Open(new Uri("Resources\\Победа.mp3", UriKind.Relative));
+            else gameOverSound.Open(new Uri("Resources\\Проигрыш.mp3", UriKind.Relative));
+            gameOverSound.Play();
+
+            MessageBox.Show(mes);
+            gameOverSound.Close();
+        }
+    }
+}
